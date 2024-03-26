5a8cbcb55be17bc69daef858a0131bce267fd98f
saul <dlathf3210@naver.com>
2024-03-26 21:22:37 +0900

    update MainPage component

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,14 @@
+import {Link} from "react-router-dom"
+
 const MainPage = () => {
     return (
-        <div className=" text-3xl">
-            <div>MainPage</div>
+        <div>
+            <div className="flex">
+                <Link to={'/about'}>About 이동</Link>
+            </div>
+            <div className=" text-3xl">
+                <div>MainPage</div>
+            </div>
         </div>
     );
 }
