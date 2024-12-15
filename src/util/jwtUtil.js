1598686f578b3fc11637e410e9f6c6bf640a2a55
saul <dlathf3210@naver.com>
2024-12-15 15:00:18 +0900

    jwt refresh function

--- src/util/jwtUtil.js
+++ src/util/jwtUtil.js
@@ -1,7 +1,18 @@
 import axios from 'axios'
-import {getCookie} from "./cookieUtil"
+import {getCookie, setCookie} from "./cookieUtil"
+import { API_SERVER_HOST } from '../api/todoApi';
 
 const jwtAxios = axios.create();
+
+
+const refreshJWT = async (accessToken, refreshToken) => {
+    const host = API_SERVER_HOST
+
+    const header = {headers: {'Authorization':`Bearer ${accessToken}`}}
+    const res = await axios.get(`${host}/api/member/refresh?refreshToken=${refreshToken}`, header)
+    return res.data
+}
+
 
 // before 
 const beforeReq = (config) => {
@@ -23,7 +34,6 @@ const beforeReq = (config) => {
     }
         
     const {accessToken} = memberInfo
-    config.withCredentials = true
     config.headers.Authorization = `Bearer ${accessToken}`;
 
     return config
@@ -38,6 +48,24 @@ const requestFail = (err) => {
 // before return response
 const beoforeRes = async (res) => {
     console.log("before return response....")
+
+    const data = res.data;
+
+    if(data && data.error === 'ERROR_ACCESS_TOKEN'){
+        const memberCookieValue = getCookie('member');
+        const result = await refreshJWT(memberCookieValue.accessToken, memberCookieValue.refreshToken)
+
+        // 새로운 accessToken, refreshToken
+        memberCookieValue.accessToken = result.accessToken;
+        memberCookieValue.refreshToken = result.refreshToken;
+
+        setCookie('member', JSON.stringify(memberCookieValue), 1)
+
+        const originalRequest = res.config
+        originalRequest.headers.Authorization = `Bearer ${result.accessToken}`;
+        return await axios(originalRequest)
+    }
+
     return res
 }
 
